9d3de9bd684f06f7aff47748359c73fe59881462
Mika Tuupola <tuupola@appelsiini.net>
2010-04-14 09:02:10 +0300

    Disable select when it has only default option left. Add $.chainedTo() alias for those who like to use more english like syntax.
    $("#engine").chainedTo("#model");
    $("#gearbox").chainedTo("#engine");
    
    vs
    
    $("#engine").chained("#model");
    $("#gearbox").chained("#engine");

--- jquery.chained.js
+++ jquery.chained.js
@@ -5,22 +5,29 @@
         return this.each(function() {
             
             /* Save this to self because this changes when scope changes. */            
-            /* Create a backup for future use. */            
-            var self = this;
+            var self   = this;
             var backup = $(self).clone();
-            
+                        
             /* Should use #foo for now. */
             var parent = $(parent_selector);
-            
+                        
             parent.bind("change", function() {
                 $(self).html($(backup).html());
                 var selected = $(":selected", this).val();
+                
                 $("option", self).each(function() {
                     /* Remove unneeded items but save the default value. */
                     if (!$(this).hasClass(selected) && $(this).val() !== "") {
                         $(this).remove();
                     }                        
                 });
+                
+                /* If we have only the default value disable select. */
+                if (1 == $("option", self).size()) {
+                    $(self).attr("disabled", "disabled");
+                } else {
+                    $(self).removeAttr("disabled");
+                }
                 
                 /* Force updating the children. */
                 $(self).trigger("change");
@@ -29,5 +36,8 @@
             
         });  
     };
+    
+    /* Alias for those who like to use more English like syntax. */
+    $.fn.chainedTo = $.fn.chained;
     
 })(jQuery);
