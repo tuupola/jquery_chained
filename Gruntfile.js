38d22235f734a6cc14b799e6dcc7bcf022b5012b
Mika Tuupola <tuupola@appelsiini.net>
2013-09-19 16:55:52 +0300

    Tidy up Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,58 @@
 module.exports = function(grunt) {
-  "use strict";
+    "use strict";
   
-  grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
-    uglify : {
-      options: {
-        banner: "/*\n" +
-                " * Chained - jQuery non AJAX(J) chained selects plugin\n" +
-                " *\n" +
-                " * Copyright (c) 2010-2013 Mika Tuupola\n" +
-                " *\n" +
-                " * Licensed under the MIT license:\n" +
-                " *   http://www.opensource.org/licenses/mit-license.php\n" +
-                " *\n" +
-                " * Project home:\n" +
-                " *   http://www.appelsiini.net/projects/lazyload\n" +
-                " *\n" +
-                " * Version: <%= pkg.version %>\n" +
-                " *\n" +
-                " */\n"
-      },
-      target: {
-        files: {
-          "jquery.chained.min.js" : "jquery.chained.js",
-          "jquery.chained.remote.min.js" : "jquery.chained.remote.js"
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        uglify : {
+            options: {
+                banner: "/*\n" +
+                        " * Chained - jQuery non AJAX(J) chained selects plugin\n" +
+                        " *\n" +
+                        " * Copyright (c) 2010-2013 Mika Tuupola\n" +
+                        " *\n" +
+                        " * Licensed under the MIT license:\n" +
+                        " *   http://www.opensource.org/licenses/mit-license.php\n" +
+                        " *\n" +
+                        " * Project home:\n" +
+                        " *   http://www.appelsiini.net/projects/lazyload\n" +
+                        " *\n" +
+                        " * Version: <%= pkg.version %>\n" +
+                        " *\n" +
+                        " */\n"
+            },
+            target: {
+                files: {
+                    "jquery.chained.min.js" : "jquery.chained.js",
+                    "jquery.chained.remote.min.js" : "jquery.chained.remote.js"
+                }
+            }
+        },
+        watch: {
+            files: ["*.js", "!*.min.js" ,"test/spec/*Spec.js"],
+            tasks: ["test"],
+        },
+        jshint: {
+            files: ["*.js", "!*.min.js" ,"test/spec/*Spec.js"],
+            options: {
+                jshintrc: ".jshintrc"
+            }
+        },
+        jasmine: {
+            src: ["jquery.chained.js", "jquery.chained.remote.js"],
+            options: {
+                helpers: "test/spec/*Helper.js",
+                specs: "test/spec/*Spec.js",
+                vendor: ["test/vendor/jquery-1.9.0.js", "test/vendor/jasmine-jquery.js"]
+            }
         }
-      }
-    },
-    watch: {
-        files: ["Gruntfile.js", "jquery.chained.js",
-                "jquery.chained.remote.js", "test/spec/ChainedSpec.js",
-                "test/spec/ChainedRemoteSpec.js", "test/spec/ChainedRemoteMultipleSpec.js"],
-        tasks: ["test"],
-    },
-    jshint: {
-      files: ["Gruntfile.js", "jquery.chained.js",
-              "jquery.chained.remote.js", "test/spec/ChainedSpec.js",
-              "test/spec/ChainedRemoteSpec.js", "test/spec/ChainedRemoteMultipleSpec.js"],
-      options: {
-        jshintrc: ".jshintrc"
-      }
-    },
-    jasmine: {
-      src: ["jquery.chained.js", "jquery.chained.remote.js"],
-      options: {
-        helpers: "test/spec/*Helper.js",
-        specs: "test/spec/*Spec.js",
-        vendor: ["test/vendor/jquery-1.9.0.js", "test/vendor/jasmine-jquery.js"]
-        //        vendor: "test/vendor/**/*.js"
-      }
-    }
-  });
+    });
+    
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-jasmine");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
-  grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-contrib-jasmine");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-
-  grunt.registerTask("test", ["jshint", "jasmine"]);
-  grunt.registerTask("default", ["test", "uglify"]);
+    grunt.registerTask("test", ["jshint", "jasmine"]);
+    grunt.registerTask("default", ["test", "uglify"]);
 
 };
